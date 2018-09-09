83d1dcd518595af0cb59cd58d09fb4746fa2fdbf
inan <muhammed-inan@outlook.com>
2018-09-09 14:25:10 +0200

    add category entity

--- src/DataFixtures/FigureFixtures.php
+++ src/DataFixtures/FigureFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Category;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Figure;
@@ -14,18 +15,29 @@ class FigureFixtures extends Fixture
 
         $faker = \Faker\Factory::create('fr_FR');
 
-        for($i = 1; $i <= 5; $i++){
+        for($i = 1; $i <= 3; $i++){
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                  ->setDescription($faker->paragraph());
+
+         $manager->persist($category);
+        }
+
+        for($j = 1; $j <= 5; $j++){
             $figure = new Figure();
             $content = '<p>'.join($faker->paragraphs(5),'</p><p>').
             '</p>';
             $figure->setTitle($faker->sentence())
                    ->setContent($content)
                    ->setImage($faker->imageUrl())
-                   ->setCreateAt($faker->DateTimeBetween('-6 months'));
+                   ->setCreateAt($faker->DateTimeBetween('-6 months'))
+                   ->setCategory($category);
+
+
             $manager->persist($figure);
 
             //crée commentaire
-         for($j =  1; $j <= mt_rand(4,6); $j++) {
+         for($k =  1; $k <= mt_rand(4,6); $k++) {
            $comment = new Comment();
              $content = '<p>'.join($faker->paragraphs(2),'</p><p>').
                  '</p>';
--- src/Form/FigureType.php
+++ src/Form/FigureType.php
@@ -2,10 +2,12 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Figure;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class FigureType extends AbstractType
 {
@@ -13,6 +15,10 @@ class FigureType extends AbstractType
     {
         $builder
             ->add('title')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title'
+                ])
             ->add('content')
             ->add('image')
 
